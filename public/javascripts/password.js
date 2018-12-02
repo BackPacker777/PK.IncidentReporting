9280d13362c2f1ea758c4ed68f9d8109b9d78326
Howard Bates <hbates@northmen.org>
2018-12-01 19:10:03 -0500

    Fixed url for password main page load

--- public/javascripts/password.js
+++ public/javascripts/password.js
@@ -12,9 +12,11 @@ class password {
                 password.performFetch(9, pw, (result) => {
 					console.log(result);
                     if (result === `1`) {
+                        let host = window.location.host;
+                        window.open(`http://${host}/main.ejs`, '_self', 'status=yes');
                         // window.open('http://pk-incidents.herokuapp.com/main.ejs', '_self', 'status=yes');
                         // window.open('http://codedelegance.com:8558/main.ejs', '_self', 'status=yes');
-                        window.open('http://127.0.0.1:8558/main.ejs', '_self', 'status=yes');
+                        // window.open('http://127.0.0.1:8558/main.ejs', '_self', 'status=yes');
                     } else {
                         document.getElementById("password").value = ``;
                         alert(`INCORRECT Password!`);
